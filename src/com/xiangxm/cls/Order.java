513d5083de09ef3fd86a08a1789b752bb8fac5c2
lef <ximu470791413@163.com>
2014-12-15 11:09:40 +0800

    a

--- src/com/xiangxm/cls/Order.java
+++ src/com/xiangxm/cls/Order.java
@@ -1,6 +1,9 @@
 package com.xiangxm.cls;
 
-public class Order {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Order implements Parcelable {
 	public int _id;
 	public String senderName;
 	public String senderNum;
@@ -9,7 +12,7 @@ public class Order {
 	public String receiverNum;
 	public String receiverAddress;
 	public int weight;
-	public String volume ;
+	public String volume;
 	/**
 	 * 付款方式
 	 */
@@ -21,8 +24,8 @@ public class Order {
 	/**
 	 * //1有短信2无短信
 	 */
-	public int message;//1有短信2无短信
-	public String number ;
+	public int message;// 1有短信2无短信
+	public String number;
 	/**
 	 * 快递物品
 	 */
@@ -37,6 +40,62 @@ public class Order {
 	/**
 	 * 0未付款，1已付款
 	 */
-	public int isover;//是否确认过 0 没有确认，1确认过
+	public int isover;// 是否确认过 0 没有确认，1确认过
+
+	public Order(Parcel in) {
+		// TODO Auto-generated constructor stub
+	}
+	public Order() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + _id;
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (_id != other._id)
+			return false;
+		if (number == null) {
+			if (other.number != null)
+				return false;
+		} else if (!number.equals(other.number))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static final Parcelable.Creator<Order> CREATOR = new Parcelable.Creator<Order>() {
+		public Order createFromParcel(Parcel in) {
+			return new Order(in);
+		}
+
+		public Order[] newArray(int size) {
+			return new Order[size];
+		}
+	};
 }
-	
--- src/com/xiangxm/cls/User.java
+++ src/com/xiangxm/cls/User.java
@@ -1,9 +1,45 @@
 package com.xiangxm.cls;
 
-import java.io.Serializable;
+import com.xiangxm.sortlistview.SortAdapter;
 
-public class User implements Serializable {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
 	
+	public User(){
+		
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + _id;
+		result = prime * result
+				+ ((mobilePhone == null) ? 0 : mobilePhone.hashCode());
+		result = prime * result + privacy;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (_id != other._id)
+			return false;
+		if (mobilePhone == null) {
+			if (other.mobilePhone != null)
+				return false;
+		} else if (!mobilePhone.equals(other.mobilePhone))
+			return false;
+		if (privacy != other.privacy)
+			return false;
+		return true;
+	}
 	public int _id;
 	
 	public String username;
@@ -17,5 +53,45 @@ public class User implements Serializable {
 	public int privacy;
 	//1fasong2jieshou
 	public int senderorreceive;
-
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		// TODO Auto-generated method stub
+		dest.writeInt(_id);
+		dest.writeString(username);
+		dest.writeString(userpwd);
+		dest.writeString(mobilePhone);
+		dest.writeString(address);
+		dest.writeInt(privacy);
+		dest.writeInt(senderorreceive);
+	}
+	
+	public static final Parcelable.Creator<User> CREATOR = new Creator<User>() {
+		
+		@Override
+		public User[] newArray(int size) {
+			// TODO Auto-generated method stub
+			return new User[size];
+		}
+		
+		@Override
+		public User createFromParcel(Parcel source) {
+			// TODO Auto-generated method stub
+			
+			return new User(source);
+		}
+	};
+	private User (Parcel source){
+		_id = source.readInt();
+		username = source.readString();
+		userpwd = source.readString();
+		mobilePhone = source.readString();
+		address = source.readString();
+		privacy = source.readInt();
+		senderorreceive = source.readInt();
+	}
 }
